679ac1f9655e55a1657f9da086a02b8ab2436707
oksanaglu <105308639+oksanaglu@users.noreply.github.com>
2022-09-27 22:40:26 +0300

    hw-06-js-end

--- js/task-07.js
+++ js/task-07.js
@@ -1,23 +1,23 @@
-// const inputRowEl = document.querySelector("#font-size-control");
-// const inputTextEl = document.querySelector("#text");
+const inputRowEl = document.querySelector("#font-size-control");
+const inputTextEl = document.querySelector("#text");
   
-// const handleInput = () => {
-//   inputTextEl.style.fontSize = `${inputRowEl.value}px`;
-// };
+const handleInput = () => {
+  inputTextEl.style.fontSize = `${inputRowEl.value}px`;
+};
 
-// inputRowEl.addEventListener("input", handleInput);
-
-
+inputRowEl.addEventListener("input", handleInput);
 
 
-const refs = {
-  input: document.querySelector("#font-size-control"),
-  nameLabel: document.querySelector("#text"),
-}
 
-refs.input.addEventListener("input", onInputChange);
 
-function onInputChange(event) {
-  console.log(event.currentTarget.value)
-  refs.nameLabel.textContent = event.currentTarget.value;
-}
+// // const refs = {
+// //   input: document.querySelector("#font-size-control"),
+// //   nameLabel: document.querySelector("#text"),
+// // }
+
+// // refs.input.addEventListener("input", onInputChange);
+
+// // function onInputChange(event) {
+// //   console.log(event.currentTarget.value)
+// //   refs.nameLabel.textContent = event.currentTarget.value;
+// }
