7c11d20b2972f871b35fb6a72f5ec270bdf2b485
oksanaglu <105308639+oksanaglu@users.noreply.github.com>
2022-10-01 20:17:44 +0300

    update-task-10-js

--- js/task-10.js
+++ js/task-10.js
@@ -1,36 +1,33 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const create = document.querySelector('[data-create]');
+const destroy = document.querySelector('[data-destroy]');
+const boxes = document.getElementById("boxes");
+create.addEventListener("click", getAmount);
+destroy.addEventListener("click", destroyBoxes);
+
+function getAmount() {
+  const amount = +document.querySelector("#controls input").value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  const basicSize = 30;
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    const size = basicSize + i * 10;
+    const div = document.createElement("div");
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
+    fragment.appendChild(div);
+  }
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
 }
 
 
-// const create = document.querySelector('[data-create]');
-// const destroy = document.querySelector('[data-destroy]');
-// const boxes = document.getElementById("boxes");
-// create.addEventListener("click", getAmount);
-// destroy.addEventListener("click", destroyBoxes);
+function random() {
+  return Math.floor(Math.random() * 256);
+}
 
-// function getAmount() {
-//   const amount = +document.querySelector("#controls input").value;
-//   createBoxes(amount);
-// }
-
-// function createBoxes(amount) {
-//   const basicSize = 30;
-//   const fragment = document.createDocumentFragment();
-//   for (let i = 0; i < amount; i++) {
-//     const size = basicSize + i * 10;
-//     const div = document.createElement("div");
-//     div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
-//     fragment.appendChild(div);
-//   }
-//   boxes.appendChild(fragment);
-// }
-
-// function destroyBoxes() {
-//   boxes.innerHTML = "";
-// }
-
-
-// function random() {
-//   return Math.floor(Math.random() * 256);
-// }
